ad5839181d6b5b3eace535f311d97fa12414cb5b
Nitinna <91372720+Nitinna@users.noreply.github.com>
2024-09-04 16:57:15 +0530

    update price on candle

--- Fina code.py
+++ Fina code.py
@@ -3,20 +3,11 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objects as go
-import datetime
-import pdb
 import pickle
 
-
-
+# Load data
 file_name = open('iv_chart', 'rb')
-df = pickle.load(file_name) 
-
-
-
-
-
-
+df = pickle.load(file_name)
 df['datetime'] = pd.to_datetime(df['datetime'])
 
 app = dash.Dash(__name__)
@@ -37,12 +28,12 @@ app.layout = html.Div([
 def update_chart(n):
     global df
 
+    # Reload data
     file_name = open('iv_chart', 'rb')
-    df = pickle.load(file_name) 
-
+    df = pickle.load(file_name)
     df['datetime'] = pd.to_datetime(df['datetime'])
 
-
+    # Create candlestick chart
     fig = go.Figure(data=[go.Candlestick(
         x=df['datetime'],
         open=df['open'],
@@ -51,10 +42,32 @@ def update_chart(n):
         close=df['close']
     )])
 
-    fig.update_layout(title='Candlestick Chart', xaxis_title='Datetime', yaxis_title='Price',autosize=True,height=1080, width=1920)
+    # Get the latest price
+    latest_price = df.iloc[-1]['close']
+    latest_datetime = df.iloc[-1]['datetime']
+
+
+    # Add a text annotation for the latest price
+    fig.add_trace(go.Scatter(
+        x=[latest_datetime],
+        y=[latest_price],
+        mode='markers+text',
+        marker=dict(color='black', size=1),
+        text=[f': {latest_price:.2f}'],
+        textposition='top right',
+        name='PRICE'
+    ))
+
+    fig.update_layout(
+        title='Candlestick Chart',
+        xaxis_title='Datetime',
+        yaxis_title='Price',
+        autosize=True,
+        height=1080,
+        width=1920
+    )
+
     return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
