8a59298cb14dcef27899854b60daa655e462ec0c
Nitinna <91372720+Nitinna@users.noreply.github.com>
2024-12-26 12:49:41 +0530

    add number to show last numbers candle data

--- Fina code.py
+++ Fina code.py
@@ -22,16 +22,26 @@ app.layout = html.Div([
         id='interval-component',
         interval=1*1000,  # Update every second
         n_intervals=0
-    )
+    ),
+    html.Div([
+        html.Label('Number of Candles to Show:'),
+        dcc.Input(id='num-candles', type='number', value=10, min=1, step=1),
+        html.Button('Update Candles', id='update-candles-button', n_clicks=0)
+    ], style={'padding': '20px'}),
 ])
 
 @app.callback(
     Output('candlestick-chart', 'figure'),
     Input('interval-component', 'n_intervals'),
+    Input('update-candles-button', 'n_clicks'),
+    State('num-candles', 'value'),  # Get the value from the input
     State('candlestick-chart', 'relayoutData')  # Capture current xaxis range
 )
-def update_chart(n, relayoutData):
+def update_chart(n, n_clicks, num_candles, relayoutData):
     df = load_data()
+
+    # Display the last 'num_candles' number of candles
+    df = df.tail(int(num_candles))
 
     # Create candlestick chart
     fig = go.Figure(data=[go.Candlestick(
